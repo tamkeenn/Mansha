81617a34b26c9f22d1cd7910b4bff216cd4f3afd
Waseem Basheer <princewasee43@gmail.com>
2023-02-26 12:19:21 +0530

    sabiq

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import data from "./data";
 function App() {
   return (
     <div>
-      <header >
-       <a href="/" >MANSHA</a>
+      <header>
+        <a href="/">MANSHA</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.images} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
